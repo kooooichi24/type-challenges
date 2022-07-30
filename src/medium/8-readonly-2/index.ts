c3e6e95c0577025a2124c1728ae1f9747b0273d8
kooooichi24 <ex.kouichi1224@gmail.com>
2022-07-30 20:35:49 +0900

    fix: 8

--- src/medium/8-readonly-2/index.ts
+++ src/medium/8-readonly-2/index.ts
@@ -40,6 +40,8 @@ type _MyReadonly2<T, K extends keyof T = keyof T> = {
   -readonly [P in keyof T as P extends K ? never : P]: T[P];
 };
 
+type __MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & Readonly<Pick<T, K>>;
+
 type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & {
   readonly [P in K]: T[P];
 };
