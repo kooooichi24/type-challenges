6a59fb013bf5ec91b0655fda152e21a28890cec0
kooooichi24 <ex.kouichi1224@gmail.com>
2022-08-12 00:18:30 +0900

    fix: 106

--- src/medium/106-trim-left/index.ts
+++ src/medium/106-trim-left/index.ts
@@ -16,26 +16,25 @@
   > View on GitHub: https://tsch.js.org/106
 */
 
-
 /* _____________ Your Code Here _____________ */
 
-type TrimLeft<S extends string> = any
-
+type Whitespace = "\n" | "\t" | " ";
+type TrimLeft<S extends string> = S extends `${Whitespace}${infer R}`
+  ? TrimLeft<R>
+  : S;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<TrimLeft<'str'>, 'str'>>,
-  Expect<Equal<TrimLeft<' str'>, 'str'>>,
-  Expect<Equal<TrimLeft<'     str'>, 'str'>>,
-  Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
-  Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
-  Expect<Equal<TrimLeft<''>, ''>>,
-  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
-]
-
-
+  Expect<Equal<TrimLeft<"str">, "str">>,
+  Expect<Equal<TrimLeft<" str">, "str">>,
+  Expect<Equal<TrimLeft<"     str">, "str">>,
+  Expect<Equal<TrimLeft<"     str     ">, "str     ">>,
+  Expect<Equal<TrimLeft<"   \n\t foo bar ">, "foo bar ">>,
+  Expect<Equal<TrimLeft<"">, "">>,
+  Expect<Equal<TrimLeft<" \n\t">, "">>
+];
 
 /* _____________ Further Steps _____________ */
 /*
@@ -43,4 +42,3 @@ type cases = [
   > View solutions: https://tsch.js.org/106/solutions
   > More Challenges: https://tsch.js.org
 */
-
