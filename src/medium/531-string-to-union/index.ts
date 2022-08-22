b7f18773e2c8a252e247f646c7829ddcc14ed343
kooooichi24 <ex.kouichi1224@gmail.com>
2022-08-23 00:06:12 +0900

    fix: 531

--- src/medium/531-string-to-union/index.ts
+++ src/medium/531-string-to-union/index.ts
@@ -17,23 +17,26 @@
   > View on GitHub: https://tsch.js.org/531
 */
 
-
 /* _____________ Your Code Here _____________ */
 
-type StringToUnion<T extends string> = T extends '' ? never : T;
-
+type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}`
+  ? First | StringToUnion<Rest>
+  : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<StringToUnion<''>, never>>,
-  Expect<Equal<StringToUnion<'t'>, 't'>>,
-  Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
-  // Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
-]
-
-
+  Expect<Equal<StringToUnion<"">, never>>,
+  Expect<Equal<StringToUnion<"t">, "t">>,
+  Expect<Equal<StringToUnion<"hello">, "h" | "e" | "l" | "l" | "o">>,
+  Expect<
+    Equal<
+      StringToUnion<"coronavirus">,
+      "c" | "o" | "r" | "o" | "n" | "a" | "v" | "i" | "r" | "u" | "s"
+    >
+  >
+];
 
 /* _____________ Further Steps _____________ */
 /*
@@ -41,4 +44,3 @@ type cases = [
   > View solutions: https://tsch.js.org/531/solutions
   > More Challenges: https://tsch.js.org
 */
-
